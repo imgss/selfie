1892b46230c39159cba93128c5440057f53a65a4
imgss <838632526@qq.com>
2020-02-15 18:24:01 +0800

    整理代码结构

--- sketch.js
+++ sketch.js
@@ -1,16 +1,15 @@
-import {isInRect} from './utils'
+import {isInRect, Rect} from './utils'
 const $ = document.querySelector.bind(document)
 const W = window.innerWidth
 const H = window.innerHeight
 const rectWidth = 0.25 * W
-const rectPositions = [[30, 20], [10  + rectWidth * 2, 20], [20 + rectWidth, 80]]
+const rectPositions = [[30, 20], [10 + rectWidth * 2, 20], [20 + rectWidth, 80]]
 let capPosition = rectPositions[0] // 在哪个相框显示视频
-const pics = [] //照片数组
-let capture
 
+const rects = rectPositions.map(posi => new Rect(posi, rectWidth))
 window.setup = function() {
   createCanvas(W * 0.8, H);
-  capture = createCapture(VIDEO);
+  window.capture = createCapture(VIDEO);
   capture.hide();
   $('.painter').appendChild($('canvas'))
   background(240);
@@ -23,25 +22,19 @@ window.setup = function() {
 }
 
 window.draw = function() {
-  if (capture.loadedmetadata) {
-    // mirror https://github.com/processing/p5.js/issues/525
-    push();
-    translate(capPosition[0] + rectWidth, 0);
-    scale(-1.0,1.0);
-    let c = capture.get(0, 0, capture.height, capture.height);
-    image(c, 0, capPosition[1], rectWidth, rectWidth);
-    pop();
-  }
+  rects.forEach(rect => {
+    rect.draw()
+  })
 }
 
 window.doubleClicked = function(event) {
   console.log(event);
   let point = [event.clientX, event.clientY]
-  for (let rect of rectPositions) {
-    if (isInRect(point, rect, rectWidth)) {
-      capPosition = rect
-      this.console.log(rect)
-      break
+  for (let rect of rects) {
+    if (isInRect(point, rect.posi, rectWidth)) {
+      rect.isCaptured = true
+    } else {
+      rect.isCaptured = false
     }
   }
 }
@@ -53,3 +46,4 @@ function initEvent() {
     }, 3000)
   })
 }
+
--- utils.js
+++ utils.js
@@ -5,3 +5,35 @@ export function isInRect(point, rectPosi, width) {
   const bottom = rectPosi[1] + width
   return point[0] < right && point[0] > left && point[1] < bottom && point[1] > top
 }
+
+export class Rect {
+  constructor(posi, width) {
+    this.posi = posi
+    this.x = posi[0]
+    this.y = posi[1]
+    this.width = width
+    this.isCaptured = false
+    this.image = null
+  }
+
+  draw() {
+    rect(this.x, this.y, this.width, this.width);
+    if (this.image) {
+      image(this.image, this.x, this.y, this.width, this.width)
+      return
+    }
+    if (this.isCaptured && capture.loadedmetadata) {
+      // mirror https://github.com/processing/p5.js/issues/525
+      push();
+      translate(this.x + this.width, 0);
+      scale(-1.0, 1.0);
+      let c = capture.get(0, 0, capture.height, capture.height);
+      image(c, 0, this.y, this.width, this.width);
+      pop();
+    }
+  }
+
+  save() {
+    this.image = capture.get(0, 0, capture.height, capture.height);
+  }
+}
